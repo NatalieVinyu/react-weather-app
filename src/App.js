0c89cbe8fc03fe663d0ccd31ce9f71d0ef60cbdd
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2024-02-22 23:38:41 +0200

    Added styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Weather from "./Weather";
+
 
 function App() {
   return (
     <div className="App">
       <div className="container" >
-
-        <h1>Hello React</h1>
+        <Weather />
         <footer>
           This project was coded by Natlie Vinyu and is {" "}
           <a href="https://github.com/NatalieVinyu/react-weather-app" target="_blank"> open-sourced on GitHub</a>
---
+++ src/Weather.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+    return (
+        <div className="Weather">
+            <h1>Weather App</h1>
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                        <input type="search" placeholder="Enter city.." className="form-control"
+                        autoFocus="on" />
+                    </div>
+                    <div className="col-3">
+                        <input type="submit" value="Search" className="btn btn-primary" />
+                    </div>
+                </div>
+                
+            </form>
+            <h2>Tokyo</h2>
+            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"/>
+            <ul>
+                <li>Cloudy</li>
+                <li>20°C</li>
+                <li>Cloudy</li>
+            </ul>
+        </div>
+    )
+}
