0311fe5c5f4a414f0333747d7a3a21676572e2b0
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2024-02-22 22:33:22 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container" >
+
         <h1>Hello React</h1>
         <footer>
           This project was coded by Natlie Vinyu and is {" "}
           <a href="https://github.com/NatalieVinyu/react-weather-app" target="_blank"> open-sourced on GitHub</a>
         </footer>
-      </header>
+        </div>
     </div>
   );
 }
