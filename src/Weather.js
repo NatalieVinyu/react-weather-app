2673444f304ffdf00e81bda8163a98fde74684f4
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2024-02-24 18:56:51 +0200

    Updated icon

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import Date from "./Date";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
     const [data, setData] = useState({ ready: false });
+    const [city, setCity] = useState(props.defaultCity);
     function handleResponse(response) {
         console.log(response.data)
         setData({
@@ -12,23 +13,39 @@ export default function Weather(props) {
             humidity: response.data.main.humidity,
             temperature: response.data.main.temp,
             description: response.data.weather[0].description,
-            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             wind: response.data.wind.speed,
             city: response.data.name,
             date: new Date(response.data.dt * 1000)
-            
         })
+    }
+
+    function search() {
+        const apiKey = "77cb0df8b340d241d54524527e9a1295";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search(city);
+        
+    }
+
+    function handleCityChange(event) {
+        setCity(event.target.value)
     }
 
     if(data.ready) {
         return (
             <div className="Weather">
                 <h1>Weather App</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-9">
                             <input type="search" placeholder="Enter city.." className="form-control"
-                            autoFocus="off" />
+                            autoFocus="off"
+                            onChange={handleCityChange} />
                         </div>
                         <div className="col-3">
                             <input type="submit" value="Search" className="btn btn-primary" />
@@ -36,25 +53,13 @@ export default function Weather(props) {
                     </div>
                     
                 </form>
-                <h2>{data.city}</h2>
+                <WeatherInfo info={data} />
 
-                <img src={data.iconUrl}
-                alt={data.description} />
-                <div className="Description">
-                    <span className="temp">{Math.round(data.temperature)}°C</span>
-                    <span>
-                        <Date date={data.date} />
-                    </span>
-                    <span>Humidity: {data.humidity}%</span>
-                    <span>{data.description} | {data.wind}km/h</span> 
-                </div>
+               
             </div>
         )
     } else {
-        const apiKey = "77cb0df8b340d241d54524527e9a1295";
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(handleResponse)
-
+        search();
         return "Loading.."
     }
 
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,1 +1,21 @@
 import React from "react";
+import Date from "./Date";
+
+
+export default function WeatherInfo(props) {
+    return (
+        <div className="WeatherInfo">
+            <h2>{props.info.city}</h2>
+            <img src={props.info.iconUrl}
+            alt={props.info.description} />
+            <div className="Description">
+            <span className="temp">{Math.round(props.info.temperature)}°C</span>
+            <span>
+            <Date date={props.info.date} />
+            </span>
+            <span>Humidity: {props.info.humidity}%</span>
+            <span>{props.info.description} | {props.info.wind}km/h</span> 
+            </div>
+        </div>
+    ) 
+}
