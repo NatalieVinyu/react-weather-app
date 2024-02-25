ee1995a207dae53edb308710d82428a4fbd5c758
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2024-02-25 22:58:09 +0200

    react comment

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,6 @@ export default function Weather(props) {
     const [data, setData] = useState({ ready: false });
     const [city, setCity] = useState(props.defaultCity);
     function handleResponse(response) {
-        console.log(response.data)
         setData({
             ready: true, 
             humidity: response.data.main.humidity,
