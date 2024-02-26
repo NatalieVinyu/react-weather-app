70d79ad654de3430bc5fc7b060f81fecebf138d5
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2024-02-26 17:01:10 +0200

    Update icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
 
     return (
         <ReactAnimatedWeather
-        icon={codeMapping[props.code]}
+        icon={codeMapping[props.code.icon]}
         color='black'
         size={props.size}
         animate={true}
